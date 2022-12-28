08f401856fc8bfb31ef838e0c05dab3cd3322aea
titi7454 <konstantin19961996@abv.bg>
2022-12-28 19:40:25 +0100

    new commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "df4ca2d908mshf78ce0718607d9ep1143c2jsnf8137c267bfb",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6b67fc772c4c10a6903e93a78e5724b2";
